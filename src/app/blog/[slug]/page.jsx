439e4f7700789495ec1de2c88cc9a94e11fd721c
SageUA <panchyshyn33@gmail.com>
2024-01-23 17:53:20 +0200

    Add connect to MongoDB

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -26,23 +26,22 @@ const SinglePostPage = async ({ params }) => {
 
    return (
       <div className={styles.container}>
-         <div className={styles.imgContainer}>
-            <Image className={styles.img} src="/contact.png" alt="" fill />
-         </div>
+         {post.img && (<div className={styles.imgContainer}>
+            <Image className={styles.img} src={post.img} alt="" fill />
+         </div>)}
          <div className={styles.textContainer}>
             <h1 className={styles.title}>{post?.title}</h1>
             <div className={styles.detail}>
-               <Image className={styles.avatar} src="/contact.png" alt="" width={50} height={50} />
                {post && <Suspense fallback={<div>Loading...</div>}>
                   <PostUser userId={post.userId} />
                </Suspense>}
                <div className={styles.detailText}>
                   <span className={styles.detailTitle}>Published</span>
-                  <span className={styles.detailValue}>19.01.2024</span>
+                  <span className={styles.detailValue}>{post.createdAt.toString().slice(4, 16)}</span>
                </div>
             </div>
             <div className={styles.content}>
-               {post.body}
+               {post.desc}
             </div>
          </div>
       </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import Footer from '@/components/footer/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
+  title: 'Next App Title',
+  description: 'Next.js starter app description',
 }
 
 export default function RootLayout({ children }) {
